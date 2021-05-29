d23d085c88330a3b38c9875c84ed44c7c7572136
Doctor1112 <dimazaharov0@mail.ru>
2021-05-30 01:06:19 +0500

    posts

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=300)
+    post_text = models.TextField()
+    post_image = models.ImageField(upload_to='event_images/')
+    data = models.DateTimeField()
